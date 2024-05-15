189029e7894b124eefa34ea4ba643fe133504d4f
NastyaShapenkova <wondernasty@yandex.ru>
2024-05-15 14:11:22 +0500

    new

--- src/scripts/dev/main.js
+++ src/scripts/dev/main.js
@@ -1,5 +1,6 @@
 (function () {
    "use strict";
+
    //навигационное меню
    const root = document.documentElement;
    const navToggle = document.querySelector("#js-navToggle");
@@ -37,6 +38,27 @@
    });
    }
 
+
+   const swipers = document.querySelectorAll(".js-swiper");
+swipers.forEach(function (swpr) {
+  new Swiper(swpr, {
+    updateOnWindowResize: true,
+    slidesPerView: "auto",
+    freeMode: true,
+    spaceBetween: 0,
+    speed: 500,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".swiper-arrow-next",
+      prevEl: ".swiper-arrow-prev",
+      disabledClass: "arrow--disabled"
+    }
+  });
+});
 
 })();
 
