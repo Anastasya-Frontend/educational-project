57e4e1c72c2e0f2d61f5d207da6e136262862b1c
NastyaShapenkova <wondernasty@yandex.ru>
2024-06-11 14:37:22 +0500

    make a form

--- dist/scripts/jquery.ajax_event.js
+++ dist/scripts/jquery.ajax_event.js
@@ -20,6 +20,19 @@
             success: function() {
                alert("Столик успешно забронирован!");
                reserveForm[0].reset();
+
+               const redTotalCostEl = $('#redTotalCost');
+               const redTicketsItem = $('#redTickets');
+               const blackTotalCostEl = $('#blackTotalCost');
+               const blackTicketsItem = $('#blackTickets');
+               const totalCostElement = $('#totalCost');
+
+               totalCostElement.text('0'); 
+               redTotalCostEl.text('0');
+               blackTotalCostEl.text('0');
+               redTicketsItem.text('0');
+               blackTicketsItem.text('0');
+
             },
             error: function() {
                alert("Что-то пошло не так, попробуйте еще раз!");
